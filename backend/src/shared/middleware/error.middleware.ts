efc5053f52ac8d4554d3e6792c91af494cddb224
unknown <Maksim>
2020-03-13 18:40:35 +0200

    starting of working on registration

--- backend/src/shared/middleware/error.middleware.ts
+++ backend/src/shared/middleware/error.middleware.ts
@@ -1,8 +1,12 @@
 import {NextFunction, Request, Response} from "express";
 import {HttpExeption} from "../exeptions/HttpExeption";
+import {UserWithThisEmailAlreadyExist} from "../exeptions/UserExist.exeption";
 
 export const errorMiddleware = ( error:HttpExeption, req:Request, res:Response, next:NextFunction)=>{
     const status = error.status || 500;
     const message = error.message || "something went wrong";
     res.status(status).send({error:message})  
+}
+export const errorMiddlewareUserExist = (error:UserWithThisEmailAlreadyExist, req:Request, res:Response, next:NextFunction)=>{
+        res.send({error:error.message})
 }
