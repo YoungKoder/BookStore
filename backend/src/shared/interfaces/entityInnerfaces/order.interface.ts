023a1c68e9ea2fb3ba8573a29d7e8d26d82b9568
Maksym Teslenko <maksteslenko924@gmail.com>
2020-03-13 23:56:08 +0200

    creating order interface

--- backend/src/shared/interfaces/entityInnerfaces/order.interface.ts
+++ backend/src/shared/interfaces/entityInnerfaces/order.interface.ts
@@ -1,7 +1,11 @@
+import { User } from './user.interface';
 import {Document} from 'mongoose';
 import mongoose from 'mongoose';
 
 export interface Order{
-    user_id:mongoose.Schema.Types.ObjectId,
+    user_id:User,
+    description:string,
+    date:Date,
+    isRemoved:Boolean,
     items: Object
 }
