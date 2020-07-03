15f3f99eb4278b062c9b2147b9269d1a8651192e
Maxim Teslenko <maksteslenko924@gmail.com>
2020-07-03 20:02:08 +0300

    changing connection url

--- backend/src/dataAccess/database/databaseConect.ts
+++ backend/src/dataAccess/database/databaseConect.ts
@@ -3,7 +3,7 @@ import {config} from "../../config";
 
 
 export function addConnection(){
-    const url:string = "mongodb://localhost:27017/BookStoreBD";
+    const url:string = "mongodb+srv://DbBookStore:qwerty2122@cluster0.5318w.mongodb.net/BookStoreBD";
     mongoose.connect(url)
     .then(() => console.log("Conection was succes"))
     .catch((err) => console.error(err));
